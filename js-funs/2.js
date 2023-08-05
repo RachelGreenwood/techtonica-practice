d3c5328272f8505feb186b279d113a3fc8e307fb
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-08-04 21:10:46 -0500

    Writes function to count string length

---
+++ js-funs/2.js
@@ -0,0 +1,16 @@
+// Write a function that takes a string as the argument.
+// The function should return a number that is the number of characters in the string.
+// Choose names that make the code easy to understand.
+
+// Input: string
+// Output: number (number of characters in the string)
+// Return str length using length property
+
+function strLength(str) {
+    return str.length;
+}
+
+console.log(strLength("H"));
+console.log(strLength("Hi"));
+console.log(strLength("Hello World"));
+console.log(strLength(""));
