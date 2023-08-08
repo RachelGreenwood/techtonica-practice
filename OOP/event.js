c8dd2410820166e32cde0db5c4f20b9c5147668c
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-08-08 09:58:59 -0500

    Adds new objects and their values

--- OOP/event.js
+++ OOP/event.js
@@ -6,7 +6,6 @@ class Event {
     }
   }
 
-  const eventObj1 = new Event(
-    'Nothing But Thieves On Tour',
-    'Performing their new album Dead Club City'
-  );
+const eventObj1 = new Event('Nothing But Thieves', 'Dead Club City Tour');
+const eventObj2 = new Event('Hozier', 'Unreal Unearth Tour');
+const eventObj3 = new Event('Melanie Martinez', 'Portals Tour');
