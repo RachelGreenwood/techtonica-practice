45ba96510542036e2ac1fead533d98b534d28726
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-08-08 11:17:03 -0500

    Adds way to add ticket prices and types to an event

--- OOP/event.js
+++ OOP/event.js
@@ -4,6 +4,9 @@ class Event {
       this.description = description;
       this.availableTickets = [];
     }
+    addAvailableTickets(type, price) {
+        this.availableTickets.push(new TicketType(type, price));
+    }
   }
 
 
@@ -15,7 +18,8 @@ const eventArray = new Array();
 
 // pushing multiple objects to an array at once
 eventArray.push(eventObj1, eventObj2, eventObj3);
-console.log(eventArray);
+    console.log(eventObj1);
+    console.log(eventArray);
 
 document.addEventListener('DOMContentLoaded', () => {
     // Handler when the DOM is fully loaded
@@ -27,5 +31,21 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 class TicketType {
-    constructor()
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
 }
+
+eventObj1.addAvailableTickets("General Admission", 30);
+eventObj1.addAvailableTickets("Floor Seating", 50);
+    console.log(eventObj1);
+
+eventObj2.addAvailableTickets("General Admission", 25)
+eventObj2.addAvailableTickets("Floor Seating", 80)
+   console.log(eventObj2);
+
+eventObj3.addAvailableTickets("Orchestra", 300)
+eventObj3.addAvailableTickets("Mezzanine", 200)
+eventObj3.addAvailableTickets("Balcony", 100)
+   console.log(eventObj3);
