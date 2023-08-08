d6e21ad44d29c9181a25d87e9053ea7c096591d3
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-08-08 10:05:47 -0500

    Adds the objects into the array

--- OOP/event.js
+++ OOP/event.js
@@ -6,8 +6,14 @@ class Event {
     }
   }
 
+
 const eventObj1 = new Event('Nothing But Thieves', 'Dead Club City Tour');
 const eventObj2 = new Event('Hozier', 'Unreal Unearth Tour');
 const eventObj3 = new Event('Melanie Martinez', 'Portals Tour');
 
 const eventArray = new Array();
+
+// pushing multiple objects to an array at once
+eventArray.push(eventObj1, eventObj2, eventObj3);
+console.log(eventObj1, eventObj2, eventObj3);
+console.log(eventArray);
