8f158ec81e3df38e3afcfe1c80cd80c79dea39db
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-08-08 10:08:15 -0500

    Iterates over the array of objects

--- OOP/event.js
+++ OOP/event.js
@@ -15,5 +15,13 @@ const eventArray = new Array();
 
 // pushing multiple objects to an array at once
 eventArray.push(eventObj1, eventObj2, eventObj3);
-console.log(eventObj1, eventObj2, eventObj3);
 console.log(eventArray);
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Handler when the DOM is fully loaded
+    let html = '';
+    eventArray.forEach((item) => {
+      html += `<li>${item.name} - ${item.description}`;
+    });
+    document.querySelector('#event').innerHTML = html;
+  });
