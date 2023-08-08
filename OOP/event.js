4d056a8f17b98ad70ea4eb3055d784f0a15debee
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-08-08 09:59:53 -0500

    Creates an event array

--- OOP/event.js
+++ OOP/event.js
@@ -9,3 +9,5 @@ class Event {
 const eventObj1 = new Event('Nothing But Thieves', 'Dead Club City Tour');
 const eventObj2 = new Event('Hozier', 'Unreal Unearth Tour');
 const eventObj3 = new Event('Melanie Martinez', 'Portals Tour');
+
+const eventArray = new Array();
