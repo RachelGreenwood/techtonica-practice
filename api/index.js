ddd6108c7af5ebc2670241a5e4a421440a834ad9
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-08-14 15:10:21 -0500

    Adds ABV, volume, and first brew date to each beer

--- api/index.js
+++ api/index.js
@@ -1,27 +1,36 @@
 // Anonymous function for retrieving and displaying a random beer
 const grabRandomBeer = () => {
-  // Fetching random beer data from API
-  fetch("https://api.punkapi.com/v2/beers/random")
-    .then(response => response.json())
-    .then(beers => {
-      // API returns an array containg only one element: we get it
-      const beer = beers[0];
-      // Creating DOM element for some beer properties
-      const nameElement = document.createElement("h2");
-      nameElement.textContent = beer.name;
-      const descriptionElement = document.createElement("p");
-      descriptionElement.textContent = beer.description;
-      // Clear previous beer data
-      const beerElement = document.getElementById("beer");
-      beerElement.innerHTML = "";
-      // Add beer info to the page
-      beerElement.appendChild(nameElement);
-      beerElement.appendChild(descriptionElement);
-    })
-    .catch(err => {
-      console.error(err.message);
-    });
-};
-
-// Grab a new beer when clicking the button
-document.getElementById("grabButton").addEventListener("click", grabRandomBeer);
+    // Fetching random beer data from API
+    fetch("https://api.punkapi.com/v2/beers/random")
+      .then(response => response.json())
+      .then(beers => {
+        // API returns an array containg only one element: we get it
+        const beer = beers[0];
+        // Creating DOM element for some beer properties
+        const nameElement = document.createElement("h2");
+        nameElement.textContent = beer.name;
+        const descriptionElement = document.createElement("p");
+        descriptionElement.textContent = beer.description;
+        const ABV = document.createElement("p");
+        ABV.textContent = `Alcohol by Volume: ${beer.abv}%`;
+        const vol = document.createElement("p");
+        vol.textContent = `Volume: ${beer.volume.value} ${beer.volume.unit}`;
+        const first = document.createElement("p");
+        first.textContent = `First brewed on ${beer.first_brewed}`;
+        // Clear previous beer data
+        const beerElement = document.getElementById("beer");
+        beerElement.innerHTML = "";
+        // Add beer info to the page
+        beerElement.appendChild(nameElement);
+        beerElement.appendChild(descriptionElement);
+        beerElement.appendChild(ABV);
+        beerElement.appendChild(vol)
+        beerElement.appendChild(first);
+      })
+      .catch(err => {
+        console.error(err.message);
+      });
+  };
+  
+  // Grab a new beer when clicking the button
+  document.getElementById("grabButton").addEventListener("click", grabRandomBeer);
